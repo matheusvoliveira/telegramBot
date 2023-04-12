3ec6aa5d4f85c24e680e4f55bc77e70eed653c9e
Matheus Soares <matheusbraganca321@hotmail.com>
2023-04-12 00:27:48 -0300

    update project

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def start_command(update, context):
 def help_command(update, context):
     update.message.reply_text('Try typping anything and i will respond!')
 
-def custom_command(update_context):
+def custom_command(update, context):
     update.message.reply_text('This is a custom command!')
 
 def handle_response(text: str) -> str:
-    if 'hello' in the text:
-        return 'Hey there' \
+    if 'hello' in  text:
+        return 'Hey there'
     if 'how are you' in text:
         return 'I am good, thanks'
-    else
+    else:
         return 'Idk'
 def handle_message(update, context):
     message_type = update.message.chat.type
@@ -34,4 +34,29 @@ def handle_message(update, context):
 
     update.message.reply_text(response)
 
-    #teste
+def error(update, context):
+    print(f'Update {update} caused error: {context.error}')
+
+if __name__ == '__main__':
+    updater = Updater(keys.token, use_context=True)
+    dp = updater.dispatcher
+
+    #Commands
+    dp.add_handler(CommandHandler('start', start_command))
+    dp.add_handler(CommandHandler('help', help_command))
+    dp.add_handler(CommandHandler('custom', custom_command))
+
+    # o handler é um objeto que define como seu bot deve
+    # responder uma mensagem especifica como por exemplo start_command
+
+    #Messages
+    dp.add_handler(MessageHandler(Filters.text,handle_message))
+
+    #Errors
+    dp.add_error_handler(error)
+
+    #Run bot
+    updater.start_polling(1.0)
+    updater.idle()
+
+
